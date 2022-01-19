7be81a9bb5d4f5ab2b955dd58df3673279b685f9
Connor O'Hara <connor@switchboard.xyz>
2022-01-19 15:36:23 -0600

    skeleton

--- programs/oracle-solana-example/src/lib.rs
+++ programs/oracle-solana-example/src/lib.rs
@@ -1,14 +1,32 @@
 use anchor_lang::prelude::*;
+use switchboard_aggregator::decimal::SwitchboardDecimal;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod oracle_solana_example {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn update_btc_price(ctx: Context<UpdateBTCPrice>) -> ProgramResult {
+        Ok(())
+    }
+
+    pub fn init_btc_price(ctx: Context<InitBTCPrice>) -> ProgramResult {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitBTCPrice<'info> {
+    #[account(zero)]
+    pub btc_price_account: AccountLoader<'info, BTCPriceAccountData>
+}
+
+#[derive(Accounts)]
+pub struct UpdateBTCPrice {
+}
+
+#[account(zero_copy)]
+pub struct BTCPriceAccountData {
+    pub val: SwitchboardDecimal,
+}
